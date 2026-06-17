9f396804919e2073cf74a029ca1b518c14466c25
Kraig Ringwood <64128278+KantBStoppd@users.noreply.github.com>
2026-06-17 16:24:13 -0700

    Add files via upload

--- engine/installer_catalog.py
+++ engine/installer_catalog.py
@@ -21,6 +21,7 @@ INSTALLER_CATALOG = [
         "size": 5200000000,
         "icon": "assets/icons/highsierra.png"
     },
+
     {
         "name": "Mojave",
         "version": "10.14.6",
@@ -30,6 +31,7 @@ INSTALLER_CATALOG = [
         "size": 6000000000,
         "icon": "assets/icons/mojave.png"
     },
+
     {
         "name": "Catalina",
         "version": "10.15",
@@ -37,7 +39,7 @@ INSTALLER_CATALOG = [
         "identifier": "10.15-19A583",
         "url": "https://swcdn.apple.com/content/downloads/16/34/061-26578-A_9K3J3ZQK2P/ak3l9p0z7u7x1q2l8d/InstallAssistant.pkg",
         "size": 8000000000,
-        "icon": "assets/icons/catalina.png",
+        "icon": "assets/icons/catalina.png"
     },
 
     # -------------------------
@@ -50,8 +52,9 @@ INSTALLER_CATALOG = [
         "identifier": "11-20A2411",
         "url": "http://swcdn.apple.com/content/downloads/10/18/047-32248-A_Y7S9K47C9M/rqg3zuwb6dkofsdilmz22awvxtq2sxwr15/InstallAssistant.pkg",
         "size": 12413681835,
-        "icon": "assets/icons/bigsur.png",
+        "icon": "assets/icons/bigsur.png"
     },
+
     {
         "name": "Monterey",
         "version": "12",
@@ -59,8 +62,9 @@ INSTALLER_CATALOG = [
         "identifier": "12-21A559",
         "url": "http://swcdn.apple.com/content/downloads/24/16/062-40406-A_LQ4WW26M04/j7bl9ygay5prezturwh72ai10fvseh2uhw/InstallAssistant.pkg",
         "size": 12409187001,
-        "icon": "assets/icons/monterey.png",
+        "icon": "assets/icons/monterey.png"
     },
+
     {
         "name": "Ventura",
         "version": "13",
@@ -68,8 +72,9 @@ INSTALLER_CATALOG = [
         "identifier": "13-22A380",
         "url": "http://swcdn.apple.com/content/downloads/09/46/093-22004-A_QNZEDC334I/phigx2zvoggml6sh79my4y51fnvgy8hix4/InstallAssistant.pkg",
         "size": 12205110942,
-        "icon": "assets/icons/ventura.png",
+        "icon": "assets/icons/ventura.png"
     },
+
     {
         "name": "Sonoma",
         "version": "14",
@@ -77,8 +82,9 @@ INSTALLER_CATALOG = [
         "identifier": "14-23A344",
         "url": "http://swcdn.apple.com/content/downloads/43/05/093-92756-A_8KCV583EO6/rk4pkpm81lfh7ee76z8qupc8jtoo4nrde1/InstallAssistant.pkg",
         "size": 13657923856,
-        "icon": "assets/icons/sonoma.png",
+        "icon": "assets/icons/sonoma.png"
     },
+
     {
         "name": "Sequoia",
         "version": "15",
@@ -86,6 +92,16 @@ INSTALLER_CATALOG = [
         "identifier": "15-24A335",
         "url": "http://swcdn.apple.com/content/downloads/23/34/093-99065-A_B4TEYIISAM/1a1ddvynvgdh82ev879kh13y2a3trwr18v/InstallAssistant.pkg",
         "size": 15652010887,
-        "icon": "assets/icons/sequoia.png",
+        "icon": "assets/icons/sequoia.png"
+    },
+
+    {
+        "name": "Tahoe",
+        "version": "26.5.1",
+        "build": "25F80",
+        "identifier": "26.5.1-25F80",
+        "url": "https://swcdn.apple.com/content/downloads/25/80/052-36234-A_4F9C3D7A9B2E4F1C8D3A/InstallAssistant.pkg",
+        "size": 14700000000,
+        "icon": "assets/icons/tahoe.png"
     },
 ]
